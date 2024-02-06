1c83ba16f55b38df027d6204d1c2691b375405cc
JaredN <jared.bozh@gmail.com>
2024-02-07 03:01:23 +0800

    webpack_upd

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,24 +1,23 @@
-import type webpack from 'webpack';
+import webpack from 'webpack';
 import path from 'path';
 
-import buildPlugins from './buildPlugins';
-import buildLoaders from './buildLoaders';
-import buildResolvers from './buildResolvers';
-
-import { type BuildOptions } from './types/config';
-import buildDevServer from './buildDevServer';
+import { BuildOptions } from './types/config';
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
 	const { mode, paths, isDev } = options;
 
 	return {
 		mode,
-
 		entry: paths.entry,
 		output: {
 			filename: '[name].[contenthash].js',
 			path: paths.build,
 			clean: true,
+			publicPath: '/',
 		},
 
 		plugins: buildPlugins(options),
--- config/build/loaders/ScssLoader.ts
+++ config/build/loaders/ScssLoader.ts
@@ -5,29 +5,34 @@ export function buildScssLoader(isDev: boolean): webpack.RuleSetRule {
 	return {
 		test: /\.s[ac]ss$/i,
 		use: [
-			//   style-loader - Стилизует код, MiniCssExtractPlugin - раскидывает на отдельные файлы scss files в проде
-			isDev
-				? 'style-loader'
-				: MiniCssExtractPlugin.loader,
+			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
 			{
 				loader: 'css-loader',
 				options: {
 					modules: {
-						auto: (
-							resPath: string
-						) =>
-							Boolean(
-								resPath.includes(
-									'.module.'
-								)
-							),
-						localIdentName: isDev
-							? '[path].[name]__[local]--[hash:base64:5]'
-							: '[hash:base64:8]',
+						auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+						localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
 					},
 				},
 			},
 			'sass-loader',
 		],
 	};
+	// return {
+	// 	test: /\.s[ac]ss$/i,
+	// 	use: [
+	// 		//   style-loader - Стилизует код, MiniCssExtractPlugin - раскидывает на отдельные файлы scss files в проде
+	// 		isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+	// 		{
+	// 			loader: 'css-loader',
+	// 			options: {
+	// 				modules: {
+	// 					auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+	// 					localIdentName: isDev ? '[path].[name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
+	// 				},
+	// 			},
+	// 		},
+	// 		'sass-loader',
+	// 	],
+	// };
 }
